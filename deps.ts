d0af88c60f0328a6c1bfce1bdd8acd9118106d5c
kawarimidoll <kawarimidoll+git@gmail.com>
2021-07-08 10:43:20 +0900

    add cliffy ansi image

--- deps.ts
+++ deps.ts
@@ -1,7 +1,14 @@
-import {
+export {
   assert,
   assertEquals,
   assertThrows,
 } from "https://deno.land/std@0.99.0/testing/asserts.ts";
 
-export { assert, assertEquals, assertThrows };
+// export * as cowsay from "https://cdn.skypack.dev/cowsay";
+
+export {
+  ansi,
+  colors,
+  cursorTo,
+  tty,
+} from "https://deno.land/x/cliffy@v0.19.2/ansi/mod.ts";
--- main.ts
+++ main.ts
@@ -1,6 +1,22 @@
-import { Logger } from "./logger.ts";
+import { ansi } from "./deps.ts";
 
-Logger.debug("This log is debug!");
-Logger.info("This log is info!");
-Logger.warning("This log is warning!");
-Logger.error("This log is error!");
+// console.log(ansi.cursorUp.cursorLeft.eraseDown());
+// console.log(ansi.cursorUp(2).cursorLeft.eraseDown(2).toString());
+// await Deno.stdout.write(ansi.cursorUp.cursorLeft.eraseDown.toBuffer());
+
+const response = await fetch(
+  "https://storage.googleapis.com/zenn-user-upload/avatar/2379ac8d86.jpeg",
+);
+const imageBuffer: ArrayBuffer = await response.arrayBuffer();
+
+console.log(
+  ansi.cursorTo(0, 0) +
+    ansi.eraseDown() +
+    ansi.image(imageBuffer, {
+      width: 29,
+      preserveAspectRatio: true,
+    }) +
+    "\n" +
+    ansi.link("Deno Land", "https://deno.land") +
+    "\n",
+);
