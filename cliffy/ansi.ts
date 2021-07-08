01128163be17f40b7bf13a07bf1292cf4903aaf9
kawarimidoll <kawarimidoll+git@gmail.com>
2021-07-08 13:58:33 +0900

    add cliffy scripts

---
+++ cliffy/ansi.ts
@@ -0,0 +1,22 @@
+import { ansi } from "../deps.ts";
+
+// console.log(ansi.cursorUp.cursorLeft.eraseDown());
+// console.log(ansi.cursorUp(2).cursorLeft.eraseDown(2).toString());
+// await Deno.stdout.write(ansi.cursorUp.cursorLeft.eraseDown.toBuffer());
+
+const response = await fetch(
+  "https://storage.googleapis.com/zenn-user-upload/avatar/2379ac8d86.jpeg",
+);
+const imageBuffer: ArrayBuffer = await response.arrayBuffer();
+
+console.log(
+  ansi.cursorTo(0, 0) +
+    ansi.eraseDown() +
+    ansi.image(imageBuffer, {
+      width: 29,
+      preserveAspectRatio: true,
+    }) +
+    "\n" +
+    ansi.link("Deno Land", "https://deno.land") +
+    "\n",
+);
