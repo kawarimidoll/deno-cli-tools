f47620802e1ea955061d9c05b24fc38da9de8863
kawarimidoll <kawarimidoll+git@gmail.com>
2021-07-12 16:51:45 +0900

    fix denotree cli

--- denotree/cli.ts
+++ denotree/cli.ts
@@ -1,17 +1,18 @@
 import { parse } from "./deps.ts";
 import { denotree } from "./mod.ts";
 
-const {
-  a,
-  d,
-  L,
-  u,
-  h,
-  _: [dir = "."],
-} = parse(Deno.args);
+try {
+  const {
+    a,
+    d,
+    L,
+    u,
+    h,
+    _: [dir = "."],
+  } = parse(Deno.args);
 
-if (h) {
-  const msg = `denotree
+  if (h) {
+    const msg = `denotree
   'tree' powered by Deno
 
   USAGE
@@ -24,35 +25,38 @@ if (h) {
     L=num : Limit depth
     h     : Show this help message
   `;
-  console.log(msg);
-  Deno.exit(0);
-}
+    console.log(msg);
+    Deno.exit(0);
+  }
 
-const skip = [];
-if (!a) {
-  skip.push(/(^|\/)\./);
-}
-if (!u) {
-  const process = Deno.run({
-    cmd: ["git", "status", "--ignored", "-s"],
-    stdout: "piped",
-    stderr: "piped",
+  const skip = [];
+  if (!a) {
+    skip.push(/(^|\/)\./);
+  }
+  if (!u) {
+    const process = Deno.run({
+      cmd: ["git", "status", "--ignored", "-s"],
+      stdout: "piped",
+      stderr: "piped",
+    });
+    const outStr = new TextDecoder().decode(await process.output());
+    process.close();
+    const ignoredList = outStr.replace(/^[^!].+$/gm, "").replace(/^!! /mg, "")
+      .split("\n").filter((item) => item);
+
+    ignoredList.forEach((str) => {
+      skip.push(new RegExp(str.replace(".", "\\.")));
+    });
+  }
+
+  await denotree(String(dir), {
+    maxDepth: L,
+    includeFiles: !d,
+    followSymlinks: false,
+    exts: undefined, // TODO: implement this
+    match: undefined, // TODO: implement this
+    skip,
   });
-  const outStr = new TextDecoder().decode(await process.output());
-  process.close();
-  const ignoredList = outStr.replace(/^[^!].+$/gm, "").replace(/^!! /mg, "")
-    .split("\n").filter((item) => item);
-
-  ignoredList.forEach((str) => {
-    skip.push(new RegExp(str.replace(".", "\\.")));
-  });
+} catch (error) {
+  console.error(`Error: ${error}`);
 }
-
-await denotree(String(dir), {
-  maxDepth: L,
-  includeFiles: !d,
-  followSymlinks: false,
-  exts: undefined, // TODO: implement this
-  match: undefined, // TODO: implement this
-  skip,
-});
